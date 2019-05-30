7a600f47e73bf533fc8430036f0135f45f1ca02c
Jose Luiz Coe <jose.souza@catho.com>
2019-05-30 03:31:39 -0300

    Criando arquivo com constantes

---
+++ starter-code/src/constants.js
@@ -0,0 +1,26 @@
+const items = [
+    {
+        imgSrc: '/images/icon1.png',
+        title: 'Declarative',
+        description: 'React makes it painless to create interactive UIs'
+    },
+    {
+        imgSrc: '/images/icon2.png',
+        title: 'Components',
+        description: 'Build encapsulated components that manage theirs state'
+    },
+    {
+        imgSrc: '/images/icon3.png',
+        title: 'Single-Way',
+        description: 'A set of immutable values are passed to the components'
+    },
+    {
+        imgSrc: '/images/icon4.png',
+        title: 'JSX',
+        description: 'Statically-typed, designed to run on modern browsers'
+    }
+]
+
+export  {
+    items,
+}
